40775019c5cd0d2c9fba1fd1b3219321c4d2b999
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-10-11 14:18:27 -0300

    Css Modules e Css global

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { Header } from "./components/Header"
 import { Post } from "./post"
+
+import './global.css'
 
 function App() {
   return (
-    <h1>
+    <div>
+      <Header />
+
       <Post 
         author="Usuário 1"
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum perferendis non doloribus. Dolores quas recusandae non porro repudiandae voluptatem iste distinctio suscipit, quaerat eos perferendis architecto doloremque nam amet? Quod!"
       />
-    </h1>
+
+      <Post 
+        author="Usuário 2"
+        content="Um novo post muito legal"
+      />
+    </div>
   )
 }
 
