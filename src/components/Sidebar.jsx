b46e2f41d7a7899555518e188fb870512bcab1aa
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-10-11 16:06:36 -0300

    Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -8,12 +10,15 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
-        <strong>Usuário 1</strong>
+        <img className={styles.avatar} src="https://github.com/RaphaelSBarros.png" />
+
+        <strong>Raphael Souza</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
