fb741bf1c545e69b4e6c562db7e25b55a970ff52
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-10-12 15:33:31 -0300

    Estilizações do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -18,6 +18,12 @@ export function Post() {
       <div className={styles.content}>
         <p>Lorem ipsum dolor sit amet</p>
         <p>Voluptatem, neque esse! Molestias possimus iure error deserunt odit eaque repudiandae qui, perferendis tenetur magni, assumenda minima debitis doloribus, magnam similique itaque.</p>
+        <p><a href="#">jane.design/doctorcare</a></p>
+        <p>
+          <a href="#"> #novoprojeto</a>
+          <a href="#"> #nlx</a>
+          <a href="#"> #rocketseat</a>
+        </p>
       </div>
     </article>
   )
